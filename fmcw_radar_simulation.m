5e15456eb9d31cd77778ac66dbf7405bbbb9219c
Ayush Bansal <ayu023ban@gmail.com>
2022-10-25 11:12:54 +0530

    Add noise and tweak parameters

--- fmcw_radar_simulation.m
+++ fmcw_radar_simulation.m
@@ -2,18 +2,20 @@ clear all;
 close all;
 clc;
 
-global c max_vel max_range Nd Nr range_res B Tchirp slope fc no_of_targets d no_of_channels
+global c max_vel max_range Nd Nr range_res B Tchirp slope fc no_of_targets d no_of_channels snr_db
+
 
 %% chip limits
 c = 3e8;
-max_range = 200;
+max_range = 100;
 range_res = 1;
-max_vel = 100; % m/s
 
 %% chirp using the requirements above.
 B = c / (2*range_res);
-Tchirp = 5.5 * 2 * (max_range/c);
+Tchirp = 20 * 2 * (max_range/c);
+TRRI = Tchirp;
 slope = B/Tchirp;
+
 
 fc= 77e9;             %carrier freq
                                                           
@@ -21,16 +23,21 @@ fc= 77e9;             %carrier freq
 Nd=128;
 
 %The number of samples on each chirp. 
-Nr=1024;
+Nr=512;
+
+vel_res = c/(2*fc*TRRI*Nd);
+max_vel = Nd/2*vel_res;
+
+snr_db = -9;
 
 
 sensors_coordinates = [0,0,0];
 sensor_directions = [0,1,0];
-target_coordinates = [110,0,0;0,80,0];
-target_velocities = [20,0,0;0,40,0];
+target_coordinates = [50,0,0];
+target_velocities = [40,0,0];
 no_of_sensors = size(sensors_coordinates,1);
 no_of_targets = size(target_coordinates,1);
-no_of_channels = 10;
+no_of_channels = 1;
 d = 2e-3;
 
 %% Calling of Functions
@@ -44,18 +51,18 @@ d = 2e-3;
      Mix = zeros(no_of_channels,length(t));
      for channel_index = 1:no_of_channels
          Mix(channel_index,:) = baseband_signal_generation(range_vector, vel_vector,theta_vector,channel_index); 
-          RDM = range_doppler_response(Mix(channel_index,:));
-          visualize_fmcw_radar_baseband_signals(RDM)
+%           RDM = range_doppler_response(Mix(channel_index,:));
+%           visualize_fmcw_radar_baseband_signals(RDM)
      end
-%      RDTM = range_doppler_theta_response(Mix);
-%      for channel_index = 1:10
-%          visualize_fmcw_radar_baseband_signals(RDTM(:,:,channel_index));
-%      end
+     RDTM = range_doppler_theta_response(Mix);
+     for channel_index = 1:no_of_channels
+         visualize_fmcw_radar_baseband_signals(RDTM(:,:,channel_index));
+     end
  end
 
 %% Baseband Signal Generation
 function Mix = baseband_signal_generation(range_vector, vel_vector,theta_vector,channel_index)
-global c range Nd Nr Tchirp slope fc no_of_targets temp d
+global c range Nd Nr Tchirp slope fc no_of_targets temp d snr_db
 % Timestamp for running the displacement scenario for every sample on each
 % chirp
 t=linspace(0,Nd*Tchirp,Nr*Nd); %total time for samples
@@ -74,6 +81,10 @@ td=zeros(1,length(t));
 weights = rand(1, no_of_targets);
 weights = weights/sum(weights);
 
+snr = 10^(snr_db/10);
+sig_pow = 1;
+noise_pow = sig_pow/snr;
+
 for target_index= 1:no_of_targets
     range = range_vector(target_index);
     vel = vel_vector(target_index);
@@ -87,10 +98,11 @@ for target_index= 1:no_of_targets
       Tx(i) = 2*pi*(fc*t(i) + (slope*t(i)^2)/2 );
       Rx(i) = 2*pi*(fc*(new_time) + (slope * (new_time)^2)/2 ) ;
       temp = Tx(i)-Rx(i);
-
       Mix(i) = Mix(i) + weight .* complex(cos(temp),sin(temp));
     end
 end
+% Adding AWGN Noise
+Mix = Mix + randn(1,length(t))*sqrt(noise_pow);
 end
 
 
@@ -128,7 +140,10 @@ global max_vel Nd Nr max_range
 doppler_axis = linspace(-max_vel,max_vel,Nd);
 range_axis = linspace(-max_range,max_range,Nr)*((Nr/2)/(max_range));
 
-figure,surf(doppler_axis,range_axis,RDM);
+figure;
+s = surf(doppler_axis,range_axis,RDM);
+s.EdgeColor = 'none';
+view(2);
 title('Amplitude and Range From FFT2');
 xlabel('Speed');
 ylabel('Range');
