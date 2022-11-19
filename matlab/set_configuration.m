3498f2c9163201ef8950c9d3e8075e6b9d9a4195
Ayush Bansal <ayu023ban@gmail.com>
2022-11-19 15:09:28 +0530

    Add another sensor and reduce channels to 4

--- matlab/set_configuration.m
+++ matlab/set_configuration.m
@@ -31,13 +31,13 @@ max_vel = Nd/(2*vel_res);
 snr_db = -9;
 
 
-sensors_coordinates = [0,0,0;30,0,0];
-sensor_directions = [0,1,0;0,1,0];
+sensors_coordinates = [0,0,0;3,0,0;4,0,0];
+sensor_directions = [0,1,0;0,1,0;0,1,0];
 target_coordinates = [50,0,0;10,20,0];
 target_velocities = [40,0,0;10,0,0];
 no_of_sensors = size(sensors_coordinates,1);
 no_of_targets = size(target_coordinates,1);
-no_of_channels = 10;
+no_of_channels = 4;
 d = 2e-3;
 
 end
